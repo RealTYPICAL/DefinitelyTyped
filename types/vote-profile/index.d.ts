54756a76933b252b63a493a0f8818cc9f61e04b1
james <jetter019@msn.com>
2018-02-12 22:19:43 +0100

    Fleshed this out a bit. Now time for testing.

--- types/vote-profile/index.d.ts
+++ types/vote-profile/index.d.ts
@@ -1,39 +1,30 @@
 // Type definitions for vote-profile x.x
-// Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
-// Definitions by: My Self <https://github.com/me>
+// Project: https://github.com/thirdcreed/vote-profile
+// Definitions by: James Tooley <https://github.com/RealTYPICAL>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7.1
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace myLib;
-
-/*~ If this module has methods, declare them as functions like so.
- */
-export function myMethod(a: string): string;
-export function myOtherMethod(a: number): number;
-
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
+export interface Vote {
+    key: string;
+    ordering: string[];
+    numVotes: number;
 }
 
-/*~ You can declare properties of the module using const, let, or var */
-export const myField: number;
+export declare class Profile {
 
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-    export function foo(): void;
+    constructor(systems: string[]);
+
+    public setAlternatives(): void;
+
+    public find(ordering: string[]): Vote[];
+
+    public vote(ballot: Vote): void;
+
+    public each(visitFunc: (ordering: string[], voteValue: number, index: number) => void): void;
+
+    public extend(addOn: string): void;
+
+    public score(scoreMethod: string): number;
+
 }
+
